b85849ce106df1e2e9c92be21f5c18b4772f8b09
ZIHAO GUO <zihaoguo@ZIHAOdeMacBook-Pro.local>
2021-09-19 15:29:56 +0800

    leetcode 19. removeNthFromEnd cpp

---
+++ linked_list/removeNthFromEnd.cpp
@@ -0,0 +1,45 @@
+// leetcode 19. removeNthFromEnd 
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) 
+    {
+        if(head == nullptr)
+        {
+            return head;
+        }
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        for (int i = 0; i < n; i++)
+        {
+            fast = fast->next;
+        }
+
+        if (!fast)
+        {
+            return head->next;
+        }
+
+        while(fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        slow->next = slow->next->next;
+        return head;
+    }
+};
