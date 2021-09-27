16c8ee3589a71810d86bd85ab84176fdafefbe54
ZIHAO GUO <zihaoguo@ZIHAOdeMacBook-Pro.local>
2021-09-27 18:52:21 +0800

    leetcode 102. levelOrder bfs

--- binary_tree/levelOrder_BFS.py
+++ binary_tree/levelOrder_BFS.py
@@ -1,62 +1,35 @@
- '''
-给定一个二叉树，返回该二叉树层序遍历的结果，（从左到右，一层一层地遍历）
-例如：
-给定的二叉树是{3,9,20,#,#,15,7},
-该二叉树层序遍历的结果是
-[
-[3],
-[9,20],
-[15,7]
-]
-'''
+# leetcode 102. levelOrder bfs
 
-# 
-# @param root TreeNode类 
-# @return int整型二维数组
-#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
 
-class Solution:
+class Solution(object):
     def levelOrder(self, root):
-        # write code here
-        queue = []
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
         if not root:
-            return queue
-        else:
-            queue.append(root)
+            return []
 
-        res = []
-        while queue:
-            temp = []
-            for i in range(len(queue)):
-                node = queue.pop(0)
-                temp.append(node.val)
+        res = [[root.val]]
+        curNodeList = [root]
+        while len(curNodeList) > 0:
+            tempNodeList = []
+            temp_res = []
+            for node in curNodeList:
                 if node.left:
-                    queue.append(node.left)
+                    tempNodeList.append(node.left)
+                    temp_res.append(node.left.val)
                 if node.right:
-                    queue.append(node.right)
-            res.append(temp)
+                    tempNodeList.append(node.right)
+                    temp_res.append(node.right.val)
+            if len(temp_res) > 0:
+                res.append(temp_res)
+            curNodeList = tempNodeList
         return res
-
-
-def create_tree(root, list, i):
-    if i < len(list):
-        if not list[i]: 
-            return None
-        else:
-            root = TreeNode(list[i])
-            root.left = create_tree(root.left, list, i*2+1)
-            root.right = create_tree(root.right, list, i*2+2)
-            return root
-    return root
-
-# Test
-nums=[2,3,4,5,6,7,None,None,None,None,None,None,8]
-root = create_tree(None, nums, 0)
-solution = Solution()
-print(solution.levelOrder(root))
