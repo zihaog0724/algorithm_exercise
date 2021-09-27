755f4f7921dd86773981d5f380936bcdbeea6a92
ZIHAO GUO <zihaoguo@ZIHAOdeMacBook-Pro.local>
2021-09-27 16:55:37 +0800

    leetcode 144. preorder

---
+++ binary_tree/preorderTraversal.py
@@ -0,0 +1,19 @@
+# leetcode 144. preorderTraversal
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
