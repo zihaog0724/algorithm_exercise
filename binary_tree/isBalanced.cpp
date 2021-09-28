f805336d0b5c8787abfa444b2f1e439f7df26ef1
ZIHAO GUO <zihaoguo@ZIHAOdeMacBook-Pro.local>
2021-09-28 12:03:07 +0800

    leetcode 110. isBalanced cpp

---
+++ binary_tree/isBalanced.cpp
@@ -0,0 +1,42 @@
+// leetcode 110. isBalanced 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) 
+    {
+        if(!root)
+        {
+            return true;
+        }
+        int left = this->getDepth(root->left);
+        int right = this->getDepth(root->right);
+        if(abs(left - right) > 1)
+        {
+            return false;
+        }
+        return this->isBalanced(root->left) && this->isBalanced(root->right);
+    }
+
+    int getDepth(TreeNode* root)
+    {
+        if(!root)
+        {
+            return 0;
+        }
+        int left = this->getDepth(root->left);
+        int right = this->getDepth(root->right);
+        return max(left, right) + 1;
+    }
+};
