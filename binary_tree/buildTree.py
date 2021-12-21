0fe82eef201ff0bb3b280467b0b97e2a1701333d
ZIHAO GUO <zihaog@alumni.cmu.edu>
2021-12-21 17:23:31 +0800

    leetcode 105. reconstruct binary tree

---
+++ binary_tree/buildTree.py
@@ -0,0 +1,23 @@
+# leetcode 105. reconstruct binary tree
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder:
+            return None
+        root = TreeNode(preorder[0])
+        root_idx = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:root_idx+1], inorder[:root_idx])
+        root.right = self.buildTree(preorder[root_idx+1:], inorder[root_idx+1:])
+        return root
