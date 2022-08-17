6a6da01209820e063b360131d658331b031fcca3
zihaog0724 <751180903@qq.com>
2022-08-17 17:36:56 +0800

    hot100-lc 23.

---
+++ leetcode_hot_100/mergeKLists.py
@@ -0,0 +1,46 @@
+# hot100-lc 23. 
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        if len(lists) == 0:
+            return []
+        if len(lists) == 1:
+            return lists[0]
+        mid = len(lists) // 2
+        left = self.mergeKLists(lists[:mid])
+        right = self.mergeKLists(lists[mid:])
+        return self.mergeTwoLists(left, right)
+
+    def mergeTwoLists(self, l1, l2):
+        ret = ListNode()
+        head = ret
+        while l1 and l2:
+            if l1.val < l2.val:
+                head.next = l1
+                head = head.next
+                l1 = l1.next
+            else:
+                head.next = l2
+                head = head.next
+                l2 = l2.next
+
+        while l1:
+            head.next = l1
+            head = head.next
+            l1 = l1.next
+
+        while l2:
+            head.next = l2
+            head = head.next
+            l2 = l2.next
+        return ret.next
