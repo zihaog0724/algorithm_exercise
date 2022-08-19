14cc679b7f35e36535409665e408e382c0119454
zihaog0724 <751180903@qq.com>
2022-08-19 16:02:23 +0800

    hot100-lc 42. 接雨水

---
+++ leetcode_hot_100/trap.py
@@ -0,0 +1,21 @@
+# hc100-lc 42. 接雨水
+
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height) - 1
+        max_left, max_right = height[l], height[r]
+        ret = 0
+        while l < r:
+            max_left = max(max_left, height[l])
+            max_right = max(max_right, height[r])
+            if max_left < max_right:
+                ret += (max_left - height[l])
+                l += 1
+            else:
+                ret += (max_right - height[r])
+                r -= 1
+        return ret
