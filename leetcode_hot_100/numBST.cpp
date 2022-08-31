651deab40016d5b5c609a143b974995f544a75bf
zihaog0724 <751180903@qq.com>
2022-08-31 19:04:03 +0800

    hot100-lc 96. 不同的二叉搜索树

---
+++ leetcode_hot_100/numBST.cpp
@@ -0,0 +1,31 @@
+// hot100-lc 96. 不同的二叉搜索树
+
+class Solution {
+public:
+    int numTrees(int n)
+    {
+        if (n < 2)
+        {
+            return 1;
+        }
+        if (n == 2)
+        {
+            return 2;
+        }
+
+        int* dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n + 1; i++)
+        {
+            dp[i] = 0;
+            for (int j = i - 1; j >= 0; j--)
+            {
+                int k = i - j - 1;
+                dp[i] += dp[j] * dp[k];
+            }
+        }
+        return dp[n];
+    }
+};
