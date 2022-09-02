5c1fa97bee0ba9e2968e64f40b268d1ced7a604f
zihaog0724 <751180903@qq.com>
2022-09-02 15:13:22 +0800

    hot100-lc 104. 二叉树maxDepth

---
+++ leetcode_hot_100/maxDepth.py
@@ -0,0 +1,22 @@
+# hot100-lc 104. 二叉树最大高度
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right) + 1
+
