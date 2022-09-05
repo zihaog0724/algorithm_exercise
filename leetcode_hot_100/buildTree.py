851cd5c88dddd328ff4944d726dd1140ebe73b3b
zihaog0724 <751180903@qq.com>
2022-09-05 14:39:00 +0800

    hot100-lc 105.

---
+++ leetcode_hot_100/buildTree.py
@@ -0,0 +1,22 @@
+# hot100-lc 105.
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if len(preorder) == 0:
+            return None
+        root = TreeNode(preorder[0])
+        idx = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:idx+1], inorder[:idx])
+        root.right = self.buildTree(preorder[idx+1:], inorder[idx+1:])
+        return root
