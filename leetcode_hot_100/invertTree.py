f033010f1df65da6cede56b16eed1a0f4376eb8b
zihaog0724 <751180903@qq.com>
2022-09-20 16:15:36 +0800

    hot100-lc 227. 翻转二叉树

---
+++ leetcode_hot_100/invertTree.py
@@ -0,0 +1,24 @@
+# hot10o-lc 226. 翻转二叉树
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        root.left = self.invertTree(root.left)
+        root.right = self.invertTree(root.right)
+        return root
