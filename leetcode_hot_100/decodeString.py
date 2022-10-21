52341873416a30e0bc84c7eb3374c5a342fe1135
zihaog0724 <751180903@qq.com>
2022-10-21 15:24:47 +0800

    hot100-lc 394. 字符串解码

---
+++ leetcode_hot_100/decodeString.py
@@ -0,0 +1,22 @@
+# hot100-lc 394. 字符串解码
+
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        for c in s:
+            if c == ']':
+                repeat_str = ''
+                repeat_cnt = ''
+                while stack[-1] != '[':
+                    repeat_str = stack.pop() + repeat_str
+                stack.pop()
+                while stack and stack[-1].isnumeric():
+                    repeat_cnt = stack.pop() + repeat_cnt
+                stack.append(repeat_str * int(repeat_cnt))
+            else:
+                stack.append(c)
+        return ''.join(stack)
