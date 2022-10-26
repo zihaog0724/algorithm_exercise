31929bf6bfaf33e105f800d39005add4be773151
zihaog0724 <751180903@qq.com>
2022-10-26 20:10:38 +0800

    hot100-lc 461. 汉明距离

---
+++ leetcode_hot_100/hammingDistance.py
@@ -0,0 +1,10 @@
+# hot100-lc 461. 汉明距离
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return bin(x ^ y).count('1')
