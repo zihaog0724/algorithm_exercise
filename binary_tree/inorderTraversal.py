1f423b85952188cc403d6dd8a44a534f2256b7bc
zihaog0724 <751180903@qq.com>
2022-10-27 15:12:16 +0800

    leetcode 94. 中序遍历

--- binary_tree/inorderTraversal.py
+++ binary_tree/inorderTraversal.py
@@ -6,12 +6,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution(object):
     def inorderTraversal(self, root):
         """
         :type root: TreeNode
         :rtype: List[int]
         """
+        self.ret = []
+        self.dfs(root)
+        return self.ret
+
+    def dfs(self, root):
         if not root:
-            return[]
-        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+            return
+        self.dfs(root.left)
+        self.ret.append(root.val)
+        self.dfs(root.right)
