182175503a567c5913260439bf65bc8f32bd63cf
zihaog0724 <751180903@qq.com>
2022-11-01 19:00:03 +0800

    hot100-lc 617. 合并二叉树

---
+++ leetcode_hot_100/mergeTrees.py
@@ -0,0 +1,27 @@
+# hot100-lc 617. 合并二叉树
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def mergeTrees(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: TreeNode
+        """
+        if not root1 and not root2:
+            return None
+        if root1 and not root2:
+            return root1
+        if not root1 and root2:
+            return root2
+
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+        return root1
