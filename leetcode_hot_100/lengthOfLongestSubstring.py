2904c1e486fbc542841b61a817aa085e73a74eac
zihaog0724 <751180903@qq.com>
2022-11-11 11:47:53 +0800

    hot100-lc 3. 无重复字符的最长子串

--- leetcode_hot_100/lengthOfLongestSubstring.py
+++ leetcode_hot_100/lengthOfLongestSubstring.py
@@ -1,4 +1,5 @@
-# hot100-3
+# hot100-3. 无重复字符的最长子串
+
 
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
@@ -6,22 +7,27 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        """
+        开一tmp数组用于存放当前遍历到的字符
+        遍历s, 如果当前字符不在tmp中,说明现在还是无重复
+        反之说明有重复了, 需要找到tmp中重复字符对应的idx,
+        把重复字符之前的都干掉, 让tmp不重复, 继续遍历
+        """
+        ret = 0
         if len(s) == 0:
-            return 0
+            return ret
+
         tmp = []
-        max_len = 0
         cur_len = 0
         for i in range(len(s)):
             if s[i] not in tmp:
                 tmp.append(s[i])
                 cur_len += 1
+                if cur_len > ret:
+                    ret = cur_len
             else:
-                if cur_len >= max_len:
-                    max_len = cur_len
-                idx = len(tmp) - 1 - tmp[::-1].index(s[i])
+                idx = tmp.index(s[i])
                 tmp = tmp[idx+1:]
                 tmp.append(s[i])
                 cur_len = len(tmp)
-        if cur_len >= max_len:
-            max_len = cur_len
-        return max_len
+        return ret
