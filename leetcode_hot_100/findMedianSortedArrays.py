c2f52a87b1c1df14121370dd0d14772b5034876d
zihaog0724 <751180903@qq.com>
2022-11-14 18:04:53 +0800

    hot100-lc 4. 寻找两个正序数组的中位数

--- leetcode_hot_100/findMedianSortedArrays.py
+++ leetcode_hot_100/findMedianSortedArrays.py
@@ -1,5 +1,4 @@
-# hot100-4
-
+# hot100-lc 4. 寻找两个正序数组的中位数
 
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
@@ -8,25 +7,32 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        l, r = 0, 0
-        merged_arr = []
-        while l < len(nums1) and r < len(nums2):
-            if nums1[l] < nums2[r]:
-                merged_arr.append(nums1[l])
-                l += 1
-            else:
-                merged_arr.append(nums2[r])
-                r += 1
-        while l < len(nums1):
-            merged_arr.append(nums1[l])
-            l += 1
-        while r < len(nums2):
-            merged_arr.append(nums2[r])
-            r += 1
-        print(merged_arr)
+        """
+        核心是把两个有序升序数组做合成, 用归并排序里的merge思路
+        """
+        merged_arr = self.merge(nums1, nums2)
         if len(merged_arr) % 2 == 0:
-            mid = len(merged_arr) // 2 - 1
-            return 0.5 * (merged_arr[mid] + merged_arr[mid+1])
+            mid = len(merged_arr) // 2
+            return (merged_arr[mid] + merged_arr[mid - 1]) * 0.5
         else:
             mid = len(merged_arr) // 2
             return merged_arr[mid]
+
+    def merge(self, nums1, nums2):
+        l, r = 0, 0
+        new_arr = []
+        while l < len(nums1) and r < len(nums2):
+            if nums1[l] < nums2[r]:
+                new_arr.append(nums1[l])
+                l += 1
+            else:
+                new_arr.append(nums2[r])
+                r += 1
+
+        while l < len(nums1):
+            new_arr.append(nums1[l])
+            l += 1
+        while r < len(nums2):
+            new_arr.append(nums2[r])
+            r += 1
+        return new_arr
