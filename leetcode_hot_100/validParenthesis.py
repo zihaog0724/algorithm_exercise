f7d3961de3302d117391351691b65af44be73ac0
zihaog0724 <751180903@qq.com>
2022-11-16 20:23:02 +0800

    hot100-lc 20. 有效的括号

--- leetcode_hot_100/validParenthesis.py
+++ leetcode_hot_100/validParenthesis.py
@@ -1,28 +1,35 @@
 # hot100-lc 20. validParenthesis
 
+
 class Solution(object):
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        hashmap = {"(": ")",
-                   "[": "]",
-                   "{": "}"}
-
-        if len(s) % 2 != 0:
-            return False
+        """
+        栈解法:
+        先做个hashmap, 把三种括号对应起来
+        只要分类讨论清楚何时压栈何时出栈即可
+        压栈三种情况:
+        (1) 栈为空
+        (2) 栈顶元素不在map.keys()里, 说明栈顶是右括号, 肯定不能被消掉, 压栈
+        (3) 栈顶元素在map.keys()里, 说明是左括号, 但新来的元素不是其对应的右括号, 消不掉, 压栈
+        出栈只有一种情况:
+        (1) 栈顶元素在map.keys()里, 说明是左括号, 且新来的元素恰好是其对应的右括号, 说明合法, 可以消掉, 出栈
+        """
+        mp = {'(': ')',
+              '[': ']',
+              '{': '}'}
 
         stack = []
         for char in s:
-            if len(stack) == 0:
+            if len(stack) == 0 or stack[-1] not in mp.keys():
                 stack.append(char)
                 continue
-            if char in hashmap.keys():
+            if char != mp[stack[-1]]:
                 stack.append(char)
+                continue
             else:
-                if stack[-1] in hashmap.keys() and hashmap[stack[-1]] == char:
-                    stack.pop()
-                else:
-                    return False
+                stack.pop()
         return len(stack) == 0
