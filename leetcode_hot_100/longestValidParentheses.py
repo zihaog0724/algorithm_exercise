7f4528482e6a818c7d57068d835f9733cbcdd303
zihaog0724 <751180903@qq.com>
2022-11-23 18:04:47 +0800

    hot100-lc 32. 最长有效括号

--- leetcode_hot_100/longestValidParentheses.py
+++ leetcode_hot_100/longestValidParentheses.py
@@ -1,4 +1,5 @@
-# hot100-lc 32. longestValidParentheses
+# hot100-lc 32. 最长有效括号
+
 
 class Solution(object):
     def longestValidParentheses(self, s):
@@ -6,18 +7,21 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        """
+        栈解法题解见https://zhuanlan.zhihu.com/p/555213495
+        """
         ret = 0
         stack = [-1]
         for i in range(len(s)):
-            if len(stack) == 1 and s[i] == ")":  # 栈长度为1且碰到右括号，更新栈底
+            if len(stack) == 1 and s[i] == ')':
                 stack[0] = i
                 continue
-            if s[i] == "(":  # 碰到左括号，无脑压栈
+            if s[i] == '(':
                 stack.append(i)
-            else:  # 栈长度不唯一且碰到右括号
-                if s[stack[-1]] == "(":  # 如果前一个是左括号
+            else:
+                if s[stack[-1]] == '(':
                     ret = max(ret, i - stack[-2])
                     stack.pop()
-                else:  # 如果前一个是右括号，继续更新栈底
+                else:
                     stack[0] = i
         return ret
